d333df3de87ddcce55098957cd5a84657849f53a
LeoMiguelSeda1 <leomiguelseda6@gmail.com>
2025-10-08 23:29:45 +0800

    refactor(backend): edit title

--- backend/app/Views/components/rp-head.php
+++ backend/app/Views/components/rp-head.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Roadmap - Claude AI</title>
+    <title>Roadmap</title>
     <style>
         * {
             margin: 0;
