81af7533d0bae480f0ba4a971d05846902b724a6
LeoMiguelSeda1 <leomiguelseda6@gmail.com>
2025-10-19 17:59:58 +0800

    fix(footer): Update signup link to use dynamic routing for consistency

--- backend/app/Views/components/lg-footer.php
+++ backend/app/Views/components/lg-footer.php
@@ -1,4 +1,4 @@
 <!-- Footer -->
             <div class="form-footer">
-                Don't have an account? <a href="signup.html">Signup here </a>
+                Don't have an account? <a href="<?= ('Signup') ?>">Signup here </a>
             </div>
